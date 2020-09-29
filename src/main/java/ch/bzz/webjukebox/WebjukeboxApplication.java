efa9087e783be8c3ec3ec874c13e1fc9f47f5c8e
Felix Reiniger <mc.nightfury34@gmail.com>
2020-09-29 14:27:51 +0200

    Add dependencies

---
+++ src/main/java/ch/bzz/webjukebox/WebjukeboxApplication.java
@@ -0,0 +1,13 @@
+package ch.bzz.webjukebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjukeboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebjukeboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bzz/webjukebox/WebjukeboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bzz.webjukebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebjukeboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
